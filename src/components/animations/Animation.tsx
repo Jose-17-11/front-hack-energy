c8c0f02c7b17b61bf3e6fb30b6b7665a49c98433
Jose-17-11 <ariashdez@outlook.com>
2024-09-12 00:05:06 -0600

    add props in component tsx for json

--- src/components/animations/Animation.tsx
+++ src/components/animations/Animation.tsx
@@ -3,92 +3,10 @@ import { BarChart } from '@mui/x-charts/BarChart';
 import { AiOutlineBgColors } from 'react-icons/ai';
 import { TbBackground } from 'react-icons/tb';
 
-export const dataset = [
-  {
-    london: 59,
-    paris: 57,
-    newYork: 86,
-    seoul: 201,
-    month: 'Enero',
-  },
-  {
-    london: 50,
-    paris: 52,
-    newYork: 78,
-    seoul: 180,
-    month: 'Febrero',
-  },
-  {
-    london: 47,
-    paris: 53,
-    newYork: 106,
-    seoul: 110,
-    month: 'Marzo',
-  },
-  {
-    london: 54,
-    paris: 56,
-    newYork: 92,
-    seoul: 73,
-    month: 'Abril',
-  },
-  {
-    london: 57,
-    paris: 69,
-    newYork: 92,
-    seoul: 99,
-    month: 'Mayo',
-  },
-  {
-    london: 60,
-    paris: 63,
-    newYork: 103,
-    seoul: 144,
-    month: 'Junio',
-  },
-  {
-    london: 59,
-    paris: 60,
-    newYork: 105,
-    seoul: 319,
-    month: 'Julio',
-  },
-  {
-    london: 65,
-    paris: 60,
-    newYork: 106,
-    seoul: 249,
-    month: 'Agosto',
-  },
-  {
-    london: 51,
-    paris: 51,
-    newYork: 95,
-    seoul: 131,
-    month: 'Septiembre',
-  },
-  {
-    london: 60,
-    paris: 65,
-    newYork: 97,
-    seoul: 55,
-    month: 'Octubre',
-  },
-  {
-    london: 67,
-    paris: 64,
-    newYork: 76,
-    seoul: 48,
-    month: 'Noviembre',
-  },
-  {
-    london: 61,
-    paris: 70,
-    newYork: 103,
-    seoul: 25,
-    month: 'Diciembre',
-  },
-];
+// Define una interfaz para los props
+interface GridDemoProps {
+  dataset: Array<{ london: number; paris: number; newYork: number; seoul: number; month: string }>;
+}
 
 const chartSetting = {
   xAxis: [
@@ -103,7 +21,8 @@ const chartSetting = {
 
 const valueFormatter = (value: number | null) => `${value}Kwh`;
 
-export default function GridDemo() {
+// Ahora tu componente recibe los props de acuerdo a la interfaz
+export default function GridDemo({ dataset }: GridDemoProps) {
   return (
     <BarChart
       dataset={dataset}
