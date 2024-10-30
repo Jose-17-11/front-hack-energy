29a1601aa7bd38e9436ec1368af79d84ecd8c5d0
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2024-10-30 00:36:48 -0600

    Fix animation mascota

--- src/components/animations/Mascota.jsx
+++ src/components/animations/Mascota.jsx
@@ -1,16 +1,16 @@
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 function Mascota(props) {
   return (
     <motion.div
-        initial={{translateX:"200%", opacity:0, rotate:90}}
-        animate={{translateX:"150%", opacity:1, rotate:0}}
-        transition={{duration:2}}
-        className='w-60 h-60 object-cover absolute top-0'
+      initial={{ x: 200, opacity: 0, rotate: 90 }} // Comienza desde la derecha con x: 200
+      animate={{ x: 30, opacity: 1, rotate: 0 }} // Se mueve hacia el centro
+      transition={{ duration: 2 }}
+      className="w-60 h-60 flex items-center justify-center"
     >
-        {props.children}
+      {props.children}
     </motion.div>
-  )
+  );
 }
 
-export default Mascota
+export default Mascota;
