b1e2b80a7ac7c68696a85ab4f47016afda9498ff
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2024-11-21 11:20:57 -0600

    get user aside

---
+++ src/components/app/UserAside.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
+
+export default function UserAside() {
+  const [userData, setUserData] = useState({
+    name: "Cargando...",
+    email: "Cargando...",
+    phone: "Cargando...",
+  });
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    console.log("userId", userId);
+
+    if (userId) {
+      fetch(`http://200.234.224.102/user/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("data", data);
+          setUserData({
+            name:
+              `${data.nombre} ${data.apellidoP} ${data.apellidoM}` ||
+              "Nombre no disponible",
+            email: data.correo || "Email no disponible",
+            phone: data.telefono || "Teléfono no disponible",
+          });
+        })
+        .catch((error) =>
+          console.error("Error al obtener los datos del usuario:", error)
+        );
+    }
+  }, []);
+
+  return (
+    <aside className="bg-CFE-600 p-4 w-64 flex flex-col items-center gap-4">
+      <div className="w-20 h-20 flex justify-center items-center mt-3">
+        <FaUserCircle className="text-9xl" />
+      </div>
+      <article className="flex flex-col items-center gap-8 text-center mt-2">
+        <h2 className="text-black mt-2 text-xl font-bold">{userData.name}</h2>
+        <ul className="text-slate-700 flex flex-col items-center gap-2 text-xl">
+          <li className="mt-2">{userData.email}</li>
+          <li className="mt-2">{userData.phone}</li>
+        </ul>
+      </article>
+    </aside>
+  );
+}
